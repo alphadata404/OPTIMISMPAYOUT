644cdee9967691c963fe7e7f09dcef0445715b37
alphadata404 <ajitkhanna408@gmail.com>
2025-05-03 03:15:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReceiptPreview from "./components/ReceiptPreview";
+
+function App() {
+  return (
+    <div className="bg-[#ececec] min-h-screen flex items-center justify-center">
+      <ReceiptPreview />
+    </div>
+  );
+}
+
+export default App;
